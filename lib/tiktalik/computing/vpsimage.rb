5e91b90aaa1950109a55c2dafeb0399315523202
Bernard Potocki <bernard.potocki@imanel.org>
2013-05-28 14:50:42 +0200

    Add full VPSImage support

--- lib/tiktalik/computing/vpsimage.rb
+++ lib/tiktalik/computing/vpsimage.rb
@@ -13,14 +13,22 @@ module Tiktalik
 
       # List of VPS images.
       def self.all
+        results = request(:get, '/computing/image')
+        results.collect { |result| new(result) }
       end
 
       # Get image or backup.
+      #
+      # @param [String] uuid UUID of image
       def self.find(uuid)
+        result = request(:get, "/computing/image/#{uuid}")
+        new(result)
       end
 
       # Delete users backup image.
       def destroy
+        request(:delete, "/computing/image/#{uuid}")
+        true
       end
 
     end
